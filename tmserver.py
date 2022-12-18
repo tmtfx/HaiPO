62de3c55b71964107c829488e311c97be63c896c
tmtfx <f.t.public@gmail.com>
2022-12-18 23:48:24 +0100

    add missing entries
    Implement missing entries in tmx file

--- tmserver.py
+++ tmserver.py
@@ -1,4 +1,4 @@
-import pickle,socket
+import pickle,socket,os
 from translate.storage.tmx import tmxfile
 from Levenshtein import distance as lev
 
@@ -20,7 +20,7 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
                     instr = client_socket.recv(HEADER)
                     if not instr:
                         break
-                    print("richiesto:",instr)
+                    #print("richiesto:",instr)
                     message = pickle.loads(instr)
                     suggerimenti=[]
                     if message==[None]:
@@ -29,6 +29,32 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
                         client_socket.sendall(packsug)
                         keeperoftheloop=False
                         break
+                    else:
+                        #try:
+                            if message[0][0]==None:
+                                print("adding source: "+message[0][1]+"\nand translation: "+message[0][2])
+                                with open("outtmx2", 'rb') as fin:
+                                    with open("outtmx3", 'a') as des:
+                                        whole=fin.read()
+                                        liniis=whole.decode("utf-8").split('\n')
+                                        for linie in liniis:
+                                            #print(str(linie))
+                                            if str(linie).find("</body>")==-1:
+                                                des.write(str(linie)+"\n")
+                                            else:
+                                                des.write("    <tu>\n      <tuv xml:lang=\"en\">\n        <seg>"+message[0][1]+"</seg>\n      </tuv>\n")
+                                                des.write("      <tuv xml:lang=\"fur\">\n        <seg>"+message[0][2]+"</seg>\n      </tuv>\n    </tu>\n")
+                                                des.write("  </body>\n</tmx>\n")
+                                                #addnewstrings(message[0][1],message[0][2])
+                                                des.close()
+                                                if os.path.exists("old_outtmx2"):
+                                                    os.remove("old_outtmx2")
+                                                os.rename("outtmx2","old_outtmx2")
+                                                os.rename("outtmx3","outtmx2")
+                                                break
+                                break
+                        #except:
+                        #    pass
                     #elif message==[alc]:
                     #    aggiungi a tmx il termine e la traduzione
                     lung1=len(message[0])
